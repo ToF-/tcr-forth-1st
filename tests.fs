c885829b2317c16119d644a27df747dc79bc1e61
Christophe Thibaut <cthibaut@octo.com>
2022-07-24 10:18:41 +0200

    added pass-or-die

--- tests.fs
+++ tests.fs
@@ -1,6 +1,14 @@
 require ffl/tst.fs
 
+: pass-or-die
+  tst-errors @ if
+    tst-errors ? ." failures" cr
+    s" failed" exception throw
+  then ;
+
 t{ ." a dummy test" cr
   true ?false
 }t
+
+pass-or-die
 bye
